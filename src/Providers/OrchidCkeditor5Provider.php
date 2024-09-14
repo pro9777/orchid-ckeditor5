14eb42fbcb3d5da617b56af52e1bde9f884e88d0
pro9777 <programmer9777@gmail.com>
2024-09-15 00:42:32 +0300

    first commit

---
+++ src/Providers/OrchidCkeditor5Provider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pro9777\OrchidCkeditor5\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrchidCkeditor5Provider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        dd(66666);
+    }
+}
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import vitejsPluginVue from '@vitejs/plugin-vue';
+
+export default {
+	server: {
+		open: 'index.html'
+	},
+	plugins: [vitejsPluginVue()]
+};
